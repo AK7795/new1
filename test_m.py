4a78c51bbe05ec04618431bd3963f4cf3c3c9ec0
AK7795 <akshaysingodi@gmail.com>
2022-03-29 17:01:39 +0530

    ftf

--- test_m.py
+++ test_m.py
@@ -2,6 +2,7 @@ import pytest
 import main
 
 
+#@pytest.mark.xfail
 @pytest.mark.parametrize("a,b,c",[(3,2,5),(1,5,6),(5,7,12)])
 def test_add(a,b,c):
     r1 = main.add(a,b)
@@ -18,6 +19,7 @@ def test_sub(a,b,c):
 def test_mul(a,b,c):
     r1 = main.mul(a,b)
     assert c == r1
+
 
 @pytest.mark.parametrize("a,b,c",[(4,2,2),(10,5,2),(15,3,5)])
 def test_div(a,b,c):
